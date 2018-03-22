44b9b2851c5322b468b89e211b161e9b6d08ee43
Pete Katsaboulas <pete@virality.me>
2018-03-22 18:19:30 -0500

    add https

--- js/main.js
+++ js/main.js
@@ -33,9 +33,7 @@ $(function(){
 
         if( $window.width() > 1110 ){
             $('.job-filter-column').show();
-        } else{
-            $('.job-filter-column').hide();
-        }
+        } 
 
         $('#mobile-nav, .mobile-nav-btn, #main').removeClass('active');
         $('body').removeClass('no-scroll');
